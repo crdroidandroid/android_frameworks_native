f81d0402d3fa2ba15f4be871ecd2e91432cf660d
Dave Kessler <activethrasher00@gmail.com>
2016-03-29 01:44:00 -0400

    Remove ATRACE from SurfaceFlinger
    Change-Id: I1764c68a9c71ac5c24a5ac4a4e7951bdaf3aa5a3
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- services/surfaceflinger/Android.mk
+++ services/surfaceflinger/Android.mk
@@ -205,7 +205,7 @@ endif
 
 LOCAL_MODULE := libsurfaceflinger
 
-LOCAL_CFLAGS += -Wall -Werror -Wunused -Wunreachable-code
+LOCAL_CFLAGS += -Wall -Werror -Wunused -Wunreachable-code -Wno-unused-private-field
 
 include $(BUILD_SHARED_LIBRARY)
 
