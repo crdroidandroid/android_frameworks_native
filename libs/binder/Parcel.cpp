88fab11d5e76ad0142f92515b4f70af1ed49a8f4
Bin Chen <pierr.chen@gmail.com>
2016-08-25 15:45:51 +1000

    binder: protected against null Parcelable
    Change-Id: I6a2cad7f504c0381dbed9e35c95b1b6cc25f4a0b
    Signed-off-by: Bin Chen <pierr.chen@gmail.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- libs/binder/Parcel.cpp
+++ libs/binder/Parcel.cpp
@@ -1939,6 +1939,9 @@ wp<IBinder> Parcel::readWeakBinder() const
 }
 
 status_t Parcel::readParcelable(Parcelable* parcelable) const {
+    if (parcelable == nullptr) {
+        return BAD_VALUE;
+    }
     int32_t have_parcelable = 0;
     status_t status = readInt32(&have_parcelable);
     if (status != OK) {
