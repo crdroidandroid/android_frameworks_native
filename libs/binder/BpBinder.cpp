5ea02753809634ceea838b531373d92dcf56fb97
Danny Lin <danny@kdrag0n.dev>
2021-04-05 22:26:06 -0700

    libbinder: Suppress log spam when unlinking death recipients
    Unlinking death recipients (from linkToDeath) is a normal part of Binder
    operation, and doing it when a BpBinder's refcount has reached 0 is not
    much different.
    
    This log message is constantly spamming when swiping as part of a back
    navigation gesture:
    
    04-05 22:29:41.402   655  3888 I BpBinder: onLastStrongRef automatically unlinking death recipients: <uncached descriptor>
    04-05 22:29:41.413   655  3888 I BpBinder: onLastStrongRef automatically unlinking death recipients: <uncached descriptor>
    04-05 22:29:41.424   655  3888 I BpBinder: onLastStrongRef automatically unlinking death recipients: <uncached descriptor>
    04-05 22:29:41.435   655   681 I BpBinder: onLastStrongRef automatically unlinking death recipients: <uncached descriptor>
    04-05 22:29:41.447   655  3888 I BpBinder: onLastStrongRef automatically unlinking death recipients: <uncached descriptor>
    
    Logging is relatively expensive on Android and the log spam isn't
    useful, so suppress this log message by setting it to the verbose log
    level.
    
    Change-Id: I774d0c59ca6f70a4e2ed33b9fac3fb5b86d8ff0a
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- libs/binder/BpBinder.cpp
+++ libs/binder/BpBinder.cpp
@@ -436,7 +436,7 @@ void BpBinder::onLastStrongRef(const void* /*id*/)
     Vector<Obituary>* obits = mObituaries;
     if(obits != nullptr) {
         if (!obits->isEmpty()) {
-            ALOGI("onLastStrongRef automatically unlinking death recipients: %s",
+            ALOGV("onLastStrongRef automatically unlinking death recipients: %s",
                   mDescriptorCache.size() ? String8(mDescriptorCache).c_str() : "<uncached descriptor>");
         }
 
