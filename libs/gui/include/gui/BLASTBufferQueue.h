3fc344c04bd473cdde58945e7177eab95328339a
Jun Wang <junwan@codeaurora.org>
2021-04-22 13:27:40 +0800

    libgui: refactor pre-rendering feature
    Change-Id: I42ff3794fb781e82f6e95ca1c319102231d782aa
    CRs-Fixed: 2927512
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- libs/gui/include/gui/BLASTBufferQueue.h
+++ libs/gui/include/gui/BLASTBufferQueue.h
@@ -85,6 +85,13 @@ public:
     sp<Surface> getSurface(bool includeSurfaceControlHandle);
     bool isSameSurfaceControl(const sp<SurfaceControl>& surfaceControl) const;
 
+    void setUndequeuedBufferCount(int count) {
+        mNumUndequeued = count;
+    }
+    int getUndequeuedBufferCount() const {
+        return mNumUndequeued;
+    }
+
     void onBufferFreed(const wp<GraphicBuffer>&/* graphicBuffer*/) override { /* TODO */ }
     void onFrameReplaced(const BufferItem& item) override;
     void onFrameAvailable(const BufferItem& item) override;
@@ -172,6 +179,7 @@ private:
     // the max to be acquired
     int32_t mMaxAcquiredBuffers = 1;
 
+    int mNumUndequeued GUARDED_BY(mMutex) =0;
     int32_t mNumFrameAvailable GUARDED_BY(mMutex) = 0;
     int32_t mNumAcquired GUARDED_BY(mMutex) = 0;
 
