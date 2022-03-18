50a264f65b38fcfc71fe474267a1220c4e1a43bf
Ray Zhang <quic_rayz@quicinc.com>
2022-03-18 15:39:24 +0800

    nativedisplay: fix the flicker issue in streaming video
    In streaming video case, video decoder needs to change buffer
    color space dynamically according to video stream real-time
    parameters.
    
    CRs-Fixed: 3155775
    Change-Id: I21590ee41708f5cb1ed45cb5a7f5c00f1c129bb0
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- libs/nativedisplay/include/surfacetexture/EGLConsumer.h
+++ libs/nativedisplay/include/surfacetexture/EGLConsumer.h
@@ -21,6 +21,7 @@
 #include <gui/BufferQueueDefs.h>
 #include <ui/FenceTime.h>
 #include <ui/GraphicBuffer.h>
+#include <ui/GraphicTypes.h>
 #include <utils/Mutex.h>
 
 namespace android {
@@ -198,7 +199,7 @@ protected:
 
         // mGraphicBuffer is the buffer that was used to create this image.
         sp<GraphicBuffer> mGraphicBuffer;
-
+        ui::Dataspace mDataSpace;
         // mEglImage is the EGLImage created from mGraphicBuffer.
         EGLImageKHR mEglImage;
 
