dd52b4599a762a27deb19fd7cca820b8265e5623
Rajat Yadav <quic_rajayada@quicinc.com>
2022-05-02 12:11:01 +0530

    surfaceflinger: Modify dumpAll to dump on the main thread
    Modify dumpAll to dump on the main thread to address the race
    condition issue pertaining to dual display use case.
    
    surfaceflinger: Modify dumpProtoFromMainThread to lock mStateLock
    
    Modify dumpProtoFromMainThread to lock mStateLock in case there
    are threads outside of main thread accessing local objects.
    
    CRs-Fixed: 3163560
    
    Change-Id: I13cad753f07c53585e377a4199d5cec538368914
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- services/surfaceflinger/SurfaceFlinger.cpp
+++ services/surfaceflinger/SurfaceFlinger.cpp
@@ -5078,6 +5078,7 @@ status_t SurfaceFlinger::doDump(int fd, const DumpArgs& args, bool asProto) {
             if (!lock.locked()) {
                 StringAppendF(&result, "Dumping without lock after timeout: %s (%d)\n",
                               strerror(-lock.status), lock.status);
+                return NO_ERROR;
             }
 
             if (const auto it = dumpers.find(flag); it != dumpers.end()) {
@@ -5318,6 +5319,7 @@ void SurfaceFlinger::dumpWideColorInfo(std::string& result) const {
 
 LayersProto SurfaceFlinger::dumpDrawingStateProto(uint32_t traceFlags) const {
     LayersProto layersProto;
+    Mutex::Autolock _l(mStateLock);
     for (const sp<Layer>& layer : mDrawingState.layersSortedByZ) {
         layer->writeToProto(layersProto, traceFlags);
     }
