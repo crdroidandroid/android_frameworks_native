8e3136342bff1c0d787e4b06f3220ece52160ef4
Juhyung Park <qkrwngud825@gmail.com>
2022-05-23 08:13:38 +0900

    SF: Scheduler: vote max for NOTIFICATION_SHADE
    We want the maximum refresh rate for pulling down the notification panel.
    
    Change-Id: I688f6f07089f1db9d39f2c33e89239480c0c782a
    Signed-off-by: Juhyung Park <qkrwngud825@gmail.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- services/surfaceflinger/Scheduler/Scheduler.cpp
+++ services/surfaceflinger/Scheduler/Scheduler.cpp
@@ -514,6 +514,9 @@ void Scheduler::registerLayer(Layer* layer) {
         windowType == WindowType::VOLUME_OVERLAY ||
         windowType == WindowType::NAVIGATION_BAR_PANEL) {
         voteType = scheduler::LayerHistory::LayerVoteType::NoVote;
+    } else if (windowType == WindowType::NOTIFICATION_SHADE) {
+        // Enforce max refresh rate for notification pulldown
+        voteType = scheduler::LayerHistory::LayerVoteType::Max;
     } else if (windowType == WindowType::WALLPAPER) {
         // Running Wallpaper at Min is considered as part of content detection.
         voteType = scheduler::LayerHistory::LayerVoteType::Min;
