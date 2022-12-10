b5da1def7f3ffcae5c5fcdda4a7d93248dbbb1b1
Pranav Vashi <neobuddy89@gmail.com>
2022-12-10 19:19:03 +0530

    Revert "sf: Check if fps scores are valid to compute best refresh rate."
    This reverts commit f1e930b262f55a2b9c783c1fd23a5580d8f7b71c.

--- services/surfaceflinger/Scheduler/RefreshRateConfigs.cpp
+++ services/surfaceflinger/Scheduler/RefreshRateConfigs.cpp
@@ -517,14 +517,7 @@ auto RefreshRateConfigs::getBestRefreshRateLocked(const std::vector<LayerRequire
             ? getMaxScoreRefreshRate(scores.rbegin(), scores.rend())
             : getMaxScoreRefreshRate(scores.begin(), scores.end());
 
-   bool noFpsScored = std::all_of(scores.begin(), scores.end(),
-                                  [](RefreshRateScore score) { return score.score == 0; });
-   if (noFpsScored) {
-      ALOGV("No fps scored - choose %s", to_string(mActiveModeIt->second->getFps()).c_str());
-      return {mActiveModeIt->second, kNoSignals};
-   }
-
-   if (primaryRangeIsSingleRate) {
+    if (primaryRangeIsSingleRate) {
         // If we never scored any layers, then choose the rate from the primary
         // range instead of picking a random score from the app range.
         if (std::all_of(scores.begin(), scores.end(),
