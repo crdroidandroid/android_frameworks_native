da9ed47bfba20a699ebfb613d920c5399529ab70
Adithya R <gh0strider.2k18.reborn@gmail.com>
2022-12-17 03:22:53 +0530

    SF: Scheduler: set an expiration for heuristic layer's high refresh rate usages
    Original change by Juhyung Park <qkrwngud825@gmail.com>,
    refactored and adapted to Android 13.
    
    Non-interactive heuristic layer's high refresh rate usages are overkill
    and unnecessary.
    
    Set a hard expiration limit to 3 seconds, or the idle timeout, whichever
    is higher.
    
    This effectively fixes/workarounds a major shortcoming in the whole
    scheduler where it's totally unaware of the view's sizes.
    
    For example, Instagram Reels with audios are played with a full-screen
    video layer and a small animating audio icon at the bottom.
    
    Because the audio icon's animations are lightweight, it's done at the
    full, highest possible refresh rate. This prevents the device from
    entering lower refresh rate states regardless of the video's fps.
    
    With this commit, such layers are not allowed to use > 60 Hz states for
    longer than 3 seconds.
    
    [gotenksIN - Adapt to 14]
    
    Change-Id: I065250bbbe3f0b9ff071779bcc43a4499463ef24
    Co-authored-by: Juhyung Park <qkrwngud825@gmail.com>
    Signed-off-by: Adithya R <gh0strider.2k18.reborn@gmail.com>
    Signed-off-by: Omkar Chandorkar <gotenksIN@aospa.co>

--- services/surfaceflinger/Scheduler/RefreshRateSelector.cpp
+++ services/surfaceflinger/Scheduler/RefreshRateSelector.cpp
@@ -785,7 +785,18 @@ auto RefreshRateSelector::getRankedFrameRatesLocked(const std::vector<LayerRequi
                 continue;
             }
 
-            const float layerScore = calculateLayerScoreLocked(layer, fps, isSeamlessSwitch);
+            float layerScore;
+            if (layer.vote == LayerVoteType::Heuristic && signals.heuristicIdle
+                    && isStrictlyLess(60_Hz, fps)) {
+                // Time for heuristic layer to keep using high refresh rate has expired
+                layerScore = 0;
+                localIsIdle = true;
+                ALOGV("%s expired to keep using %s", formatLayerInfo(layer, weight).c_str(),
+                      to_string(fps).c_str());
+            } else {
+                layerScore =
+                    calculateLayerScoreLocked(layer, fps, isSeamlessSwitch);
+            }
             const float weightedLayerScore = weight * layerScore;
 
             // Layer with fixed source has a special consideration which depends on the
