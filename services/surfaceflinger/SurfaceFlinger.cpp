36599fe9189dc294f3f22e55f768fea557e7e8f3
Ady Abraham <adyabr@google.com>
2023-03-13 11:14:14 +0800

    SF: avoid skipping waiting for the earliest time to present
    When the next expected presentation time is not allocated for
    the previous frame, SF needs to wait before submitting it to HWC
    to prevent early presentation.
    
    Bug: 273419557
    Test: tested by partner
    Test: atest ASurfaceControlTest
    Change-Id: Ia83755bbc71968369fd9176944b9a85135d8a04a
    Merged-In: Ia83755bbc71968369fd9176944b9a85135d8a04a
    
    mnri: resolve conflicts with power hint session mode
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- services/surfaceflinger/SurfaceFlinger.cpp
+++ services/surfaceflinger/SurfaceFlinger.cpp
@@ -2261,8 +2261,21 @@ void SurfaceFlinger::composite(nsecs_t frameTime, int64_t vsyncId)
     const auto expectedPresentTime = mExpectedPresentTime.load();
     const auto prevVsyncTime = mScheduler->getPreviousVsyncFrom(expectedPresentTime);
     const auto hwcMinWorkDuration = mVsyncConfiguration->getCurrentConfigs().hwcMinWorkDuration;
-    refreshArgs.earliestPresentTime = prevVsyncTime - hwcMinWorkDuration;
-    refreshArgs.previousPresentFence = mPreviousPresentFences[0].fenceTime;
+    const auto vsyncPeriod = mScheduler->getDisplayStatInfo(frameTime).vsyncPeriod;
+    const bool threeVsyncsAhead = mExpectedPresentTime - frameTime > 2 * vsyncPeriod;
+
+    // We should wait for the earliest present time if HWC doesn't support ExpectedPresentTime,
+    // and the next vsync is not already taken by the previous frame.
+    const bool waitForEarliestPresent =
+            !getHwComposer().getComposer()->isSupported(
+                    Hwc2::Composer::OptionalFeature::ExpectedPresentTime) &&
+            (threeVsyncsAhead ||
+             mPreviousPresentFences[0].fenceTime->getSignalTime() != Fence::SIGNAL_TIME_PENDING);
+
+    if (waitForEarliestPresent) {
+        refreshArgs.earliestPresentTime = prevVsyncTime - hwcMinWorkDuration;
+    }
+
     refreshArgs.scheduledFrameTime = mScheduler->getScheduledFrameTime();
     refreshArgs.expectedPresentTime = expectedPresentTime;
 
