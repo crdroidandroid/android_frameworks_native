4f90995b3b8c654062545a6a0e2aacc38ac7710f
Juhyung Park <qkrwngud825@gmail.com>
2024-08-05 16:13:00 +0000

    SF: Scheduler: maintain current refresh rate if no layers are detected
    No layers definitely doesn't mean new changes happened; so there's no
    need to enforce the maximum refresh rate. Maintain current refresh rate
    instead.
    
    [gotenksIN: adapt to 14]
    
    Change-Id: I31c9edc1a8ff3ff8340afd2f731df66755ffc9e0
    Co-authored-by: Omkar Chandorkar <gotenksIN@aospa.co>
    Signed-off-by: Omkar Chandorkar <gotenksIN@aospa.co>

--- services/surfaceflinger/Scheduler/RefreshRateSelector.cpp
+++ services/surfaceflinger/Scheduler/RefreshRateSelector.cpp
@@ -671,9 +671,10 @@ auto RefreshRateSelector::getRankedFrameRatesLocked(const std::vector<LayerRequi
         return {ranking, GlobalSignals{.idle = true}};
     }
 
+    // LMO Edit: If there are no layers, prefer to stay with the current config
     if (layers.empty() || noVoteLayers == layers.size()) {
         ALOGV("No layers with votes");
-        const auto ranking = rankFrameRates(anchorGroup, RefreshRateOrder::Descending);
+        const auto ranking = rankFrameRates(anchorGroup, RefreshRateOrder::Ascending, activeMode.getId());
         SFTRACE_FORMAT_INSTANT("%s (No layers with votes)",
                                to_string(ranking.front().frameRateMode.fps).c_str());
         return {ranking, kNoSignals};
