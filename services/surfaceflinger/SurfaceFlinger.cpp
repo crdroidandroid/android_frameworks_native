f5a0876a7b789c3637e5c9904a618b130298f5bb
minaripenguin <minaripenguin@users.noreply.github.com>
2024-08-22 11:49:40 +0800

    SF: Restore debug.sf.latch_unsignaled sysprop
    partially reverts: 1fd9bfca9d86a3b0d0ff46596ec948b147f44619
    
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- services/surfaceflinger/SurfaceFlinger.cpp
+++ services/surfaceflinger/SurfaceFlinger.cpp
@@ -557,6 +557,10 @@ LatchUnsignaledConfig SurfaceFlinger::getLatchUnsignaledConfig() {
         return LatchUnsignaledConfig::AutoSingleLayer;
     }
 
+    if (base::GetBoolProperty("debug.sf.latch_unsignaled"s, false)) {
+        return LatchUnsignaledConfig::Always;
+    }
+
     return LatchUnsignaledConfig::Disabled;
 }
 
@@ -4883,10 +4887,13 @@ TransactionHandler::TransactionReadiness SurfaceFlinger::transactionReadyBufferC
                     return TraverseBuffersReturnValues::STOP_TRAVERSAL;
                 }
 
+                // ignore the acquire fence if LatchUnsignaledConfig::Always is set.
+                const bool checkAcquireFence =
+                        enableLatchUnsignaledConfig != LatchUnsignaledConfig::Always;
                 const bool acquireFenceAvailable = s.bufferData &&
                         s.bufferData->flags.test(BufferData::BufferDataChange::fenceChanged) &&
                         s.bufferData->acquireFence;
-                const bool fenceSignaled = !acquireFenceAvailable ||
+                const bool fenceSignaled = !checkAcquireFence || !acquireFenceAvailable ||
                         s.bufferData->acquireFence->getStatus() != Fence::Status::Unsignaled;
                 if (!fenceSignaled) {
                     // check fence status
@@ -4989,6 +4996,11 @@ bool SurfaceFlinger::shouldLatchUnsignaled(const layer_state_t& state, size_t nu
     if (enableLatchUnsignaledConfig == LatchUnsignaledConfig::Disabled) {
         SFTRACE_FORMAT_INSTANT("%s: false (LatchUnsignaledConfig::Disabled)", __func__);
         return false;
+    }
+
+    if (enableLatchUnsignaledConfig == LatchUnsignaledConfig::Always) {
+        SFTRACE_FORMAT_INSTANT("%s: true (LatchUnsignaledConfig::Always)", __func__);
+        return true;
     }
 
     // We only want to latch unsignaled when a single layer is updated in this
