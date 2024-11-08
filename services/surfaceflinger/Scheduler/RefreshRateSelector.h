b71bb2139648badcf9926e2fb624af4b8fce8c4d
Adithya R <gh0strider.2k18.reborn@gmail.com>
2024-11-09 00:08:01 +0530

    SF: Fix opportunistic idling and make it more aggressive
    Also avoids touch boost with explicit vote layers as reported by
    Kazuki H <kazukih0205@gmail.com>
    
    Cleanup some redunant code while we're at it.
    
    Change-Id: If04e14cf0818902b22fc907ee88f7a4831317385
    Signed-off-by: Omkar Chandorkar <gotenksIN@aospa.co>

--- services/surfaceflinger/Scheduler/RefreshRateSelector.h
+++ services/surfaceflinger/Scheduler/RefreshRateSelector.h
@@ -227,7 +227,7 @@ public:
         }
 
         auto toString() const {
-            return ftl::Concat("{touch=", touch, ", idle=", idle,
+            return ftl::Concat("{touch=", touch, ", idle=", idle, ", heuristicIdle=", heuristicIdle,
                                ", powerOnImminent=", powerOnImminent, '}');
         }
     };
@@ -552,7 +552,6 @@ private:
 
     DisplayModeIterator mMinRefreshRateModeIt GUARDED_BY(mLock);
     DisplayModeIterator mMaxRefreshRateModeIt GUARDED_BY(mLock);
-    DisplayModeIterator mIdleRefreshRateModeIt GUARDED_BY(mLock);
 
     // Display modes that satisfy the Policy's ranges, filtered and sorted by refresh rate.
     std::vector<FrameRateMode> mPrimaryFrameRates GUARDED_BY(mLock);
